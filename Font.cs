fde83490336450202a5b7b68c91aaf833161c0a4
neverwinter-nights <71729548+neverwinter-nights@users.noreply.github.com>
2022-10-25 00:59:57 +0300

    Version 1.0

---
+++ Font.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontInstaller
+{
+    internal class Font
+    {
+        public const string RegistryPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts";
+
+        [DllImport("gdi32", EntryPoint = "AddFontResource")]
+        public static extern int AddFontResourceA(string lpFileName);
+        [DllImport("gdi32.dll")]
+        private static extern int AddFontResource(string lpszFilename);
+        [DllImport("gdi32.dll")]
+        private static extern int CreateScalableFontResource(
+            uint fdwHidden, string lpszFontRes, string lpszFontFile, string lpszCurrentPath);
+
+        public static void Install(string fontPath)
+        {
+            string fontFileName = Path.GetFileName(fontPath);
+            string osFontsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Fonts);
+            string fontDestination = Path.Combine(osFontsFolder, fontFileName);
+            string currentDir = Directory.GetCurrentDirectory();
+
+            if (File.Exists(fontDestination))
+                return;
+
+            File.Copy(Path.Combine(currentDir, fontPath), fontDestination);
+
+            PrivateFontCollection fontCol = new PrivateFontCollection();
+            fontCol.AddFontFile(fontDestination);
+            string actualFontName = fontCol.Families[0].Name;
+
+            AddFontResource(fontDestination);
+
+            Registry.SetValue(RegistryPath, actualFontName, fontFileName, RegistryValueKind.String);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontInstaller
+{
+    enum ExitCode : int
+    {
+        Success = 0,
+        FontFileIsNotSpecified = 1,
+        FontFileDoesNotExist = 2,
+        FontInstallationError = 3
+    }
+
+    internal class Program
+    {
+        static ConsoleColor ConsoleNormalColour = Console.ForegroundColor;
+        static ConsoleColor ConsoleErrorColour = ConsoleColor.Red;
+
+        static void ResetConsoleColour()
+        {
+            Console.ForegroundColor = ConsoleNormalColour;
+        }
+
+        static void LogMessage(string Message)
+        {
+            Console.ForegroundColor = ConsoleNormalColour;
+            Console.WriteLine(Message);
+        }
+
+
+        static void LogError(string Error)
+        {
+            Console.ForegroundColor = ConsoleErrorColour;
+            Console.WriteLine(Error);
+        }
+
+
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                LogError("Font file is not specified.");
+                ResetConsoleColour();
+
+                return (int)ExitCode.FontFileIsNotSpecified;
+            }
+
+            string fontArg = args[0];
+
+            if (!File.Exists(fontArg))
+            {
+                LogError("Font file does not exist.");
+                ResetConsoleColour();
+
+                return (int)ExitCode.FontFileDoesNotExist;
+            }
+
+            try
+            {
+                Font.Install(fontArg);
+            }
+            catch (Exception e)
+            {
+                LogMessage("Oops...");
+                LogError(e.ToString());
+                LogMessage("Make sure that you know what you are doing.");
+                ResetConsoleColour();
+
+                return (int)ExitCode.FontInstallationError;
+            }
+
+            return (int)ExitCode.Success;
+        }
+    }
+}
